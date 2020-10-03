bdf1a1a83347587049ccafaa03a363dd6ac2be1a
suhasagg <suhasagg@gmail.com>
2020-10-03 21:04:56 +0530

    Corda Node Diagnostics

--- corda-node-diagnosticsv1/corda-node-diagnostics/clientsv1/clients/src/main/java/com/nodeDiagnosticInfo/repository/InfoIdentifierExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.nodeDiagnosticInfo.repository;
-
-public class InfoIdentifierExistsException extends IllegalArgumentException {
-
-    public InfoIdentifierExistsException(final String identifier) {
-        super("Identifier already exists:" + identifier);
-    }
-}
